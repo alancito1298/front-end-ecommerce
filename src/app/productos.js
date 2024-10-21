6dbc2f9efe855d0aac5d7e5eae85b6eeb27f9d62
Alan <alangerman98@yahoo.com.ar>
2024-10-21 10:57:28 -0300

    modif. api

--- src/app/productos.js
+++ src/app/productos.js
@@ -25,7 +25,7 @@ function Productos() {
     // Cargar los productos desde el archivo JSON en la carpeta public
     const fetchProductos = async () => {
       try {
-        const response = await fetch('https://artlimpieza-back-hsmlbuej2-alancito1298s-projects.vercel.app');
+        const response = await fetch('https://artlimpieza-back-6aeltx3ax-alancito1298s-projects.vercel.app/producto');
         if (!response.ok) {
           throw new Error('Error al obtener los productos');
         }
